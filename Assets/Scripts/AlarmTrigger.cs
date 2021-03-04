738971ca53efc8ceebd69598dc099d824d681540
Megapartymen <pervi@list.ru>
2021-03-04 14:39:55 +0500

    Init

---
+++ Assets/Scripts/AlarmTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class AlarmTrigger : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private float _volumeDuration;
+
+    //private void Update()
+    //{
+    //    if (_audioSource.volume <= 0)
+    //    {
+    //        _audioSource.Stop();
+    //    }
+    //}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Player>(out Player player))
+        {
+            _animator.SetBool("InRestrictedArea", true);
+            _audioSource.volume = 0f;
+            _audioSource.Play();            
+            _audioSource.DOFade(1f, _volumeDuration);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent<Player>(out Player player))
+        {
+            _animator.SetBool("InRestrictedArea", false);
+            _audioSource.DOFade(0f, _volumeDuration);
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(_speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            transform.Translate(_speed * Time.deltaTime * -1, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, _speed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, _speed * Time.deltaTime * -1, 0);
+        }
+    }
+}
